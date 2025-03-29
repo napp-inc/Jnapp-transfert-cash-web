c9e081d8cdf7c13b593e824a771920ac59933f01
KUROYUKIHIME333 <dhermansum@gmail.com>
2025-03-29 18:31:51 +0100

    update

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -7,13 +7,13 @@ import React from 'react';
 import Image from 'next/image';
 
 // PreviewItems Component
-const PreviewItems = ({ src, alt, name, count }) => {
+const PreviewItems = ({ src, alt, name, count, style }) => {
 	return (
 		<div className="preview-alert flex items-center space-x-2">
 			<Image src={src} alt={alt} width={32} height={32} className="object-cover" />
 			<div className="flex flex-col">
-				<p className="text-blue-500 font-semibold">{name}</p>
-				<p className="text-blue-500 font-bold text-lg">{count}</p>
+				<p className= {style}>{name}</p>
+				<p className={style}>{count}</p>
 			</div>
 		</div>
 	);
@@ -25,13 +25,13 @@ export default function Preview() {
 		<div className="bg-gray-100 p-4 w-[100%]">
 			<h2 className="text-black-xl font-bold mb-4 title-size">Aperçu</h2>
 			<div className="preview">
-				<PreviewItems src="/dashboardIconeActive.svg" alt="Transferts en cours" name="Transferts en cours" count="15" />
+				<PreviewItems src="/transferts.svg" alt="Transferts en cours" name="Transferts en cours" count="15" style={"text-blue-500 font-bold"}  />
 
-				<PreviewItems src="/dashboardIconeActive.svg" alt="Transferts terminés" name="Transferts terminés" count="5" className="text-green-500" />
+				<PreviewItems src="/finished.svg" alt="Transferts terminés" name="Transferts terminés" count="4" style={"text-blue-500 font-bold"} />
 
-				<PreviewItems src="/dashboardIconeActive.svg" alt="Alertes en cours" name="Alertes en cours" count="4" className="text-red-500" />
+				<PreviewItems src="/alerts.svg" alt="Alertes en cours" name="Alertes en cours" count="4" style={"text-red-500 font-bold"} />
 
-				<PreviewItems src="/dashboardIconeActive.svg" alt="Transferts prévus" name="Transferts prévus" count="15" className="text-yellow-500" />
+				<PreviewItems src="/scheduled.svg" alt="Transferts prévus" name="Transferts prévus" count="15" style={"text-yellow-500 font-bold"} />
 			</div>
 		</div>
 	);
